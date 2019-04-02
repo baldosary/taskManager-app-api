feeaf53f3ef1b125763342776df394e388c5ba79
Bushra Al-dosary <baldosary@kku.edu.sa>
2019-04-02 18:38:39 +0300

    Changing the Mongodb URL

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,8 +1,5 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 
- mongoose.connect(process.env.MONGODB_URL, {
-     useNewUrlParser: true, 
-     createIndexes: true
- });
+ mongoose.connect(process.env.MONGODB_URL);
 
